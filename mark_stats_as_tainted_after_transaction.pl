e4dc03ea446191975fb8a36b764d2920fe4c80ce
rmucha <mucharafal44@gmail.com>
2021-10-29 11:40:24 +0200

    .

--- mark_stats_as_tainted_after_transaction.pl
+++ mark_stats_as_tainted_after_transaction.pl
@@ -6,7 +6,7 @@ my $info = shift;
 my $dbh_offline = $info->{dbh}->{ccdb_offline};
 my $dbh_online = $info->{dbh}->{ccdb_online};
 
-my $markStatsTableAsTainted = "update ccdb_helper_table set value = 1 where key = 'ccdb_stats_tainted';";
+my $markStatsTableAsTainted = "update ccdb_helper_table set value = 1 ;"; # where key = 'ccdb_stats_tainted'
 $dbh_offline->do($markStatsTableAsTainted);
 $dbh_online->do($markStatsTableAsTainted);
 
