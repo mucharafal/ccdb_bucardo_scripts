5ac0a3378e9697c5d27f0e681ca1047a8c5659cf
rmucha <mucharafal44@gmail.com>
2021-10-29 09:45:14 +0200

    .

--- mark_stats_as_tainted_after_transaction.pl
+++ mark_stats_as_tainted_after_transaction.pl
@@ -3,14 +3,14 @@ use warnings;
 use Data::Dumper;
 
 my $info = shift;
-# my $dbh_offline = $info->{dbh}->{ccdb_offline};
-# my $dbh_online = $info->{dbh}->{ccdb_online};
+my $dbh_offline = $info->{dbh}->{ccdb_offline};
+my $dbh_online = $info->{dbh}->{ccdb_online};
 
 my $markStatsTableAsTainted = "update ccdb_helper_table set value = 1 where key = \'ccdb_stats_tainted\';";
-# $dbh_offline->do($markStatsTableAsTainted);
-# $dbh_online->do($markStatsTableAsTainted);
+$dbh_offline->do($markStatsTableAsTainted);
+$dbh_online->do($markStatsTableAsTainted);
 
-my $file = '/tmp/bucardoDump8.txt';
+my $file = '/tmp/bucardoDump9.txt';
 
 open my $fh, '>:encoding(UTF-8)', $file or do {
 	return;
