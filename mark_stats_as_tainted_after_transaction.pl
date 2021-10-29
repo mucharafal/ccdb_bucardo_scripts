23e83d38c2257f0616b06be9d03e1d850ce30936
rmucha <mucharafal44@gmail.com>
2021-10-29 12:31:20 +0200

    Clean up script

--- mark_stats_as_tainted_after_transaction.pl
+++ mark_stats_as_tainted_after_transaction.pl
@@ -1,22 +1,12 @@
 use strict;
 use warnings;
-use Data::Dumper;
 
 my $info = shift;
 my $dbh_offline = $info->{dbh}->{ccdb_offline};
 my $dbh_online = $info->{dbh}->{ccdb_online};
 
-my $markStatsTableAsTainted = "update ccdb_helper_table set value = 1 ;"; # where key = 'ccdb_stats_tainted'
+my $markStatsTableAsTainted = "update ccdb_helper_table set value = 1 where key = 'ccdb_stats_tainted';"; 
 $dbh_offline->do($markStatsTableAsTainted);
 $dbh_online->do($markStatsTableAsTainted);
-
-my $file = '/tmp/bucardoDump11.txt';
-
-open my $fh, '>:encoding(UTF-8)', $file or do {
-	return;
-};
-
-print $fh Dumper $info;
-close $fh;
 
 return;
